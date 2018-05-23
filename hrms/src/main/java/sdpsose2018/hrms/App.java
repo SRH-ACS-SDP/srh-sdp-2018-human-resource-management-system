58f62e43d25b87be802fafad597c6a8e67fbcd47
Sebastian Strubert <sebi@strubert.de>
2018-05-23 12:18:28 +0200

    Created Initial Maven Project in eclipse

---
+++ hrms/src/main/java/sdpsose2018/hrms/App.java
@@ -0,0 +1,13 @@
+package sdpsose2018.hrms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
