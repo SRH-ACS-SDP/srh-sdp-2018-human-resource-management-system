fc79987addaaeb6af39de67837187967777a2e5a
tejveercs <39554772+tejveercs@users.noreply.github.com>
2018-05-30 15:03:31 +0200

    created employeejobassignment class

---
+++ hrms/src/main/java/sdpsose2018/hrms/Employeejobassignment.java
@@ -0,0 +1,12 @@
+package sdpsose2018.hrms;
+
+public class Employeejobassignment {
+
+	int employeeId;
+	int jobId;
+	int supervisorId;
+	double startDate;
+	double endDate;
+	int departmentId;
+	String description;
+}
