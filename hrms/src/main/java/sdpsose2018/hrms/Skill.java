ccc7030c1317f521ed15c76b2fa3c40c1b16846b
Pasindu Dasun <dasun.pasindu@yahoo.com>
2018-05-30 17:05:51 +0200

    change the variable name to id

--- hrms/src/main/java/sdpsose2018/hrms/Skill.java
+++ hrms/src/main/java/sdpsose2018/hrms/Skill.java
@@ -7,7 +7,7 @@ package sdpsose2018.hrms;
 public class Skill {
 
 
-	 int skillId;
+	 int id;
 	 String name;
 	 String description;
 	 int difficulty;
@@ -21,4 +21,3 @@ public class Skill {
 
 	}
 
-}
