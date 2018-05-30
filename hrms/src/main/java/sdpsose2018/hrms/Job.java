7f1c4ee2060498af7348fade016dda4c9968fc2d
Pasindu Dasun <dasun.pasindu@yahoo.com>
2018-05-30 15:59:54 +0200

    Created Job Class

---
+++ hrms/src/main/java/sdpsose2018/hrms/Job.java
@@ -0,0 +1,16 @@
+package sdpsose2018.hrms;
+
+public class Job {
+
+	int jobId;
+	String title;
+	String description;
+	double minSalary;
+	double maxSalary;
+	String skillsRequired;
+	
+	Job(){
+		
+		
+	}
+}
