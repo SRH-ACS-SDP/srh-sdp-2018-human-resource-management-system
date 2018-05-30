b076eeabe873dd90174b6bb31baaed0a33717a55
deepthi <srinavyavabilisetti@gmail.com>
2018-05-30 16:08:17 +0200

    Creation of Location class

---
+++ hrms/src/main/java/sdpsose2018/hrms/Location.java
@@ -0,0 +1,16 @@
+package sdpsose2018.hrms;
+
+public class Location {
+
+	int LocationId;
+	String name;
+	String address;
+	String details;
+	int CountryId;
+
+Location()
+{
+	
+}
+
+}
---
+++ hrms/src/main/java/sdpsose2018/hrms/Skill.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package sdpsose2018.hrms;
+
+
+public class Skill {
+
+
+	 int skillId;
+	 String name;
+	 String description;
+	 int difficulty;
+
+	Skill()
+	{
+		
+	}
+
+
+
+	}
+
+}
