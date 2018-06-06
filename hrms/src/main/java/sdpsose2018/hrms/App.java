9fc085cbd4b5878ad8efa2c48e1d397a2dc303b2
tejveercs <39554772+tejveercs@users.noreply.github.com>
2018-06-06 18:49:52 +0200

    updateCountry

--- hrms/src/main/java/sdpsose2018/hrms/App.java
+++ hrms/src/main/java/sdpsose2018/hrms/App.java
@@ -11,17 +11,71 @@ import javax.persistence.Persistence;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+	static Scanner sc;
+	
+	public App() {
+		sc = new Scanner(System.in);
+	}
+	
+	public void jpaProcess() {
+
     	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
     	EntityManager em = emf.createEntityManager();
+
+    	DepartmentManager departmentManager = new DepartmentManager(em, sc);
     	
-    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Choose the Field that you want to see, Please Enter the number only ");
+    	System.out.println("1 Add");
+    	System.out.println("2 View");
+    	System.out.println("3 update");
+    	System.out.println("4 Delete");
     	
-    	//ManageCourse mc = new ManageCourse(em,sc);
-    	ManageTraining mt = new ManageTraining(em,sc);
-    	mt.addTraining();
-    	//mc.addCourse();
-    	//mc.viewCourse();
+    	int  result  = sc.nextInt();
+    	
+    	switch(result) {
+    		
+    	case 1:
+    		departmentManager.addCountry(); break;
+    	case 2:
+    		departmentManager.viewCountry();break;
+    	case 3:
+    		departmentManager.updateCountry();break;
+    		
+    	case 4:
+    		departmentManager.deleteCountry();
+    	
+    		break;
+    		
+    	default:
+    			System.out.println("Error");
+    	
+    	
+    			
+    	}
+
+    	
+    	em.close();
+	}
+	
+    public static void main( String[] args )
+    {
+    	App app = new App();
+    	app.jpaProcess();
+    	
+    	String answer ;
+    	do {
+    		System.out.println("Do you want to see the changes");
+        	answer  = sc.next();
+    	
+        	if(answer.toLowerCase().equals("yes")) {
+    		app.jpaProcess();
+        	}
+    	}while(answer.toLowerCase() !="");
+    			
     }
-}
+    
+    	
+    	
+    	
+    }
+
--- hrms/src/main/java/sdpsose2018/hrms/DepartmentManager.java
+++ hrms/src/main/java/sdpsose2018/hrms/DepartmentManager.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+
 
 public class DepartmentManager {
 
@@ -13,13 +16,13 @@ public class DepartmentManager {
 	List<Department> departments;
 	
 	EntityManager em;
-	Scanner sc;
+	static Scanner sc;
 	
 	public DepartmentManager(EntityManager em, Scanner sc) {
 		this.em = em;
 		this.sc = sc;
 		
-		
+		countries = em.createQuery("from Country", Country.class).getResultList();
 	}
 	
 	public void addCountry() {
@@ -29,8 +32,9 @@ public class DepartmentManager {
 		String name;
 		
 		do {
+			System.out.println("Add Country Details");
 			System.out.print("Country name: ");
-			name = sc.nextLine();
+			name = sc.next();
 			co.setName(name);
 	    	try {
 	    		em.createQuery("from Country where name = '" + name + "'").getSingleResult();    		
@@ -59,11 +63,11 @@ public class DepartmentManager {
 		} while (invalidInput);
 			
 		System.out.print("Country language: ");
-		String language = sc.nextLine();
+		String language = sc.next();
 		co.setLanguage(language);
 	
 		System.out.print("Country currency: ");
-		String currency = sc.nextLine();
+		String currency = sc.next();
 		co.setCurrency(currency);
 		
 		
@@ -75,5 +79,94 @@ public class DepartmentManager {
 		
 		System.out.println(dbObject.toString());
 	}
+	public  void viewCountry() {
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		System.out.println("_____View all available Countries_____");
+		stringBuilder.append(String.format("%75s\n", "").replace(' ', '_'));
+		stringBuilder.append(String.format("|%-10s|%-25s|%-25s|%-10s|\n", "COUNTRY ID","NAME", "LANGUAGE", "CURRENCY").replace(' ', '_'));
+		
+		for (Country c : countries) {
+			
+			stringBuilder.append(String.format("|%10d|", c.getId()).replace(' ', '_'));
+			stringBuilder.append(String.format("%-25s|", c.getName()).replace(' ', '_'));
+			stringBuilder.append(String.format("%-25s|", c.getLanguage()).replace(' ', '_'));
+			stringBuilder.append(String.format("%-10s|", c.getCurrency()).replace(' ', '_'));
+			
+
+			stringBuilder.append("\n");
+		}
+		
+		System.out.println(stringBuilder.toString());
+	}
+	public void updateCountry() {
+		try {
+		System.out.println("Enter country name ");
+		String name  = sc.next();
+		Query query =em.createQuery("Select id from Country c where c.name LIKE :name");
+		query.setParameter("name", name);
+		
+		List id  = query.getResultList();
+		em.getTransaction().begin();
+		Country c  = em.find(Country.class,id.get(0));
+		
+		/* Now we need to update the code*/
+		
+		String name_old  = c.name;
+		String language_old  = c.language;
+		String currency_old   =  c.currency;
+		
+		updatable(name_old,language_old,currency_old,c);
+		System.out.println("Successfully Updated");
+		em.getTransaction().commit();
+		
+		}catch(Exception e) {
+			System.out.println("Errro");
+		}
+		
+		
+		
+		
+	}
 	
+	private static  void updatable(String name,String language,String currency,Country co)
+	{
+		
+		int  count  =0;
+		System.out.println("Please Enter the new name if you do not want to change the name either leave it blank or type no ");
+		String new_name =  sc.next();
+		if (new_name.equals(" ")|| new_name.toLowerCase().equals("no")) {
+			
+			co.setName(name);
+			count++;
+		}else {
+			co.setName(new_name);
+			count++;
+		}
+		
+		System.out.println("Please Enter the new language if you do not want to change the language either leave it blank or type no ");
+		String new_language=  sc.next();
+		if (new_language.equals(" ")|| new_language.toLowerCase().equals("no")) {
+			
+			co.setLanguage(language);
+		}else {
+			co.setLanguage(new_language);
+		}
+		
+		
+		System.out.println("Please Enter the new Currency if you do not want to change the currency either leave it blank or type no ");
+		String new_curreny =  sc.next();
+		if (new_curreny.equals(" ") || new_curreny.toLowerCase().equals("no")) {
+			
+			co.setCurrency(currency);
+		}else {
+			co.setCurrency(new_curreny);
+		}
+		
+		
+	}
+	
+	public void deleteCountry() {
+		
+	}
 }
