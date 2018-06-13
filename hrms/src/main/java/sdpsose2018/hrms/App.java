a279957e1754970eb3710f12c81a4504df360026
tejveercs <39554772+tejveercs@users.noreply.github.com>
2018-06-13 11:36:18 +0200

    Countries All Function Complete with Validation.
    Insert
    View
    Update
    Delete.

--- hrms/src/main/java/sdpsose2018/hrms/App.java
+++ hrms/src/main/java/sdpsose2018/hrms/App.java
@@ -1,5 +1,6 @@
 package sdpsose2018.hrms;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import javax.persistence.EntityManager;
@@ -11,135 +12,24 @@ import javax.persistence.Persistence;
  */
 public class App 
 {
-	static Scanner sc;
 	
-	public App() {
-		sc = new Scanner(System.in);
-	}
 	
-	public void country() {
-
-    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-    	EntityManager em = emf.createEntityManager();
-
-    	DepartmentManager departmentManager = new DepartmentManager(em, sc);
-    	
-    	System.out.println("Choose the Field that you want to see, Please Enter the number only ");
-    	System.out.println("1 Add");
-    	System.out.println("2 View");
-    	System.out.println("3 update");
-    	System.out.println("4 Delete");
-    	
-    	int  result  = sc.nextInt();
-    	
-    	switch(result) {
-    		
-    	case 1:
-    		departmentManager.addCountry(); break;
-    	case 2:
-    		departmentManager.viewCountry();break;
-    	case 3:
-    		departmentManager.updateCountry();break;
-    		
-    	case 4:
-    		departmentManager.deleteCountry();
-    	
-    		break;
-    		
-    	default:
-    			System.out.println("Error");
-    	
-    	
-    			
-    	}
-
-    	
-    	em.close();
-	}
 	
-	public void location() {
-
-    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-    	EntityManager em = emf.createEntityManager();
-    	
-    	
-    	
-    	DepartmentManager departmentManager = new DepartmentManager(em, sc);
-    	
-    	System.out.println("Choose the Field that you want to see, Please Enter the number only ");
-    	System.out.println("1 Add");
-    	System.out.println("2 View");
-    	System.out.println("3 update");
-    	System.out.println("4 Delete");
-    	
-    	int  result  = sc.nextInt();
-    	
-    	switch(result) {
-    		
-    	case 1:
-    		departmentManager.addLocation(); break;
-    	case 2:
-    		departmentManager.viewLocation();break;
-    	case 3:
-    		departmentManager.updateCountry();break;
-    		
-    	case 4:
-    		departmentManager.deleteCountry();
-    	
-    		break;
-    		
-    	default:
-    			System.out.println("Error");
-    	
-    	
-    			
-    	}
-
-    	
-    	em.close();
-	}
 	
-	public void ask_what()
-	{
-		
-		System.out.println("Please Enter the number in which you want to do transaction by looking in the option");
-		System.out.println("1 Country");
-		System.out.println("2 Location");
-		System.out.println("3 Department");
-		int number  =  sc.nextInt();
-		
-		switch(number) {
-		
-		case 1 : country(); DepartmentManager.d_name="Country"; break;
-		case 2:  location();DepartmentManager.d_name="Location";break;
-		case 3:  DepartmentManager.d_name="Department";break;
-		default: System.out.println("Invalid Input Try again ! ");
-		
-		}
-		
-		
-		
-	}
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-    	App app = new App();
-    	app.ask_what();
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+    	EntityManager em = emf.createEntityManager();
     	
-    	String answer ;
-    	do {
-    		System.out.println("Do you want to see the changes in this table or Go back to main menu ? enter yes or no");
-        	answer  = sc.next();
+    	Scanner sc = new Scanner(System.in);
     	
-        	if(answer.toLowerCase().equals("yes")) {
-        if (DepartmentManager.d_name.equals("Country")) {		
-    		app.country();
-        }else if(DepartmentManager.d_name.equals("Location")) {
-        	app.location();
-        }
-        	}else {
-        	app.ask_what();
-        	}
-    	}while(answer.toLowerCase() !="");
+    	DepartmentManager manager  = new DepartmentManager(em, sc);
+    	manager.call(em);
+    	
+    	
+    	em.close();
+    	
+    	
     			
     }
     
--- hrms/src/main/java/sdpsose2018/hrms/DepartmentManager.java
+++ hrms/src/main/java/sdpsose2018/hrms/DepartmentManager.java
@@ -1,6 +1,7 @@
 package sdpsose2018.hrms;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,8 @@ import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
+import antlr.StringUtils;
+
 
 
 public class DepartmentManager {
@@ -19,10 +22,12 @@ public class DepartmentManager {
 	List<Location> location;
 	List<Department> departments;
 	public static String d_name ="";
-	
+	static BufferedReader r ;
 	
 	EntityManager em;
 	static Scanner sc;
+	
+	@SuppressWarnings("unchecked")
 	
 	public DepartmentManager(EntityManager em, Scanner sc) {
 		this.em = em;
@@ -31,9 +36,11 @@ public class DepartmentManager {
 		countries = em.createQuery("from Country", Country.class).getResultList();
 		location  =  em.createQuery("from Location",Location.class).getResultList();
 		departments  = em.createQuery("from Department",Department.class).getResultList();
+		
+		r = new BufferedReader(new InputStreamReader(System.in));
 	}
 	
-	public void addCountry() {
+	public void addCountry() throws IOException {
 		Country co = new Country();
 		
 		boolean invalidInput = true;
@@ -41,15 +48,16 @@ public class DepartmentManager {
 		
 		do {
 			System.out.println("Add Country Details");
-			System.out.print("Country name: ");
+			System.out.print("Country Name: ");
 			name = sc.next();
+			if(ischeckChar(name)==true) {
 			co.setName(name);
 	    	try {
 	    		em.createQuery("from Country where name = '" + name + "'").getSingleResult();    		
-	    		System.out.println("Country already exists in Database.");
+	    		System.out.println("Country Already exists in Database.");
 	    		boolean isRetry = true;
 	    		do {
-		    		System.out.print("Try again? (y/n) ");
+		    		System.out.print("Try Again.? (y/n) ");
 		    		switch(sc.nextLine().charAt(0)) {
 		    		case 'n':
 		    			return;
@@ -68,40 +76,114 @@ public class DepartmentManager {
 	    	catch (Exception e){
 	    		System.out.println(e.getMessage());
 	    	}
+			}else {
+				System.out.println("Wrong Input!");
+			}
 		} while (invalidInput);
-			
-		System.out.print("Country language: ");
-		String language = sc.next();
-		co.setLanguage(language);
-	
-		System.out.print("Country currency: ");
-		String currency = sc.next();
-		co.setCurrency(currency);
 		
+		boolean input_type_ = true;
+		do {
+		System.out.print("Country Language: ");
+		String language = r.readLine();
+		if(ischeckChar(language)==false) {
+			System.out.println("Enter Only Strings");
+		}else {
+		co.setLanguage(language);
+		input_type_ = false;
+		}
+		}while(input_type_);
+		
+		boolean input_type = true;
+		do {
+		
+		System.out.print("Country Currency: like [USD,EUR,INR,RUS] etc ");
+		String currency = r.readLine();	
+		
+		if(currency.length() >3 || ischeckChar(currency)==false) {
+			System.out.println("Invalid input ");
+		}else {
+			co.setCurrency(currency);
+			input_type=false;
+		}
+		
+	
+		}while(input_type);
+		
+		
+		boolean input = true;
+		do {
+		try {
+			
+		
+			
+			
+		System.out.println("Enter Tax Rate");	
+		String  tax  =  r.readLine();
+		co.setTaxRate(Double.parseDouble(tax));
 		
 		em.getTransaction().begin();
+		em.flush();
+		em.clear();
 		em.persist(co);
 		em.getTransaction().commit();
 	
 		Country dbObject = em.createQuery("from Country c where c.name='" + name + "'",Country.class).getSingleResult();
 		
 		System.out.println(dbObject.toString());
+		input = false;
+		}catch(Exception e) {
+			System.out.println(" Invalid Input !");
+			
+		}
+		}while(input);
 		
 	}
+	public boolean isNumeric(String val) {
+
+		try {
+			Integer.parseInt(val);
+		}catch(Exception e) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean ischeckChar(String word) {
+		
+		char[] ch  = word.toCharArray();
+		boolean  flag  = true;
+		for (int i =0;i<ch.length;i++)
+		{
+			if(!((ch[i] >= 'a' && ch[i] <= 'z' )||(ch[i]>='A' && ch[i]<='Z')||ch[i]==' ')) {
+				
+				flag  =  false;
+				break;
+			}
+		}
+		if (flag  == false) {
+			
+			return false;
+		}else {
+		return true;
+		}
+		
+	}
+	
 	public  void viewCountry() {
 		
 		StringBuilder stringBuilder = new StringBuilder();
-		System.out.println("_____View all available Countries_____");
-		stringBuilder.append(String.format("%75s\n", "").replace(' ', '_'));
-		stringBuilder.append(String.format("|%-10s|%-25s|%-25s|%-10s|\n", "COUNTRY ID","NAME", "LANGUAGE", "CURRENCY").replace(' ', '_'));
-		
-		for (Country c : countries) {
+		System.out.println("_____View All Available Countries_____");
+		stringBuilder.append(String.format("%135s\n", "").replace(' ', '_'));
+		stringBuilder.append(String.format("|%-10s|%-50s|%-50s|%-10s|%-10s|\n", "COUNTRY ID","NAME", "LANGUAGE", "CURRENCY","TAX RATE").replace(' ', '_'));
+		TypedQuery<Country> query = em.createQuery("SELECT c FROM Country c", Country.class);
+		List<Country> results = query.getResultList();		
+		for (int i =0;i<results.size();i++) {
 			
-			stringBuilder.append(String.format("|%10d|", c.getId()).replace(' ', '_'));
-			stringBuilder.append(String.format("%-25s|", c.getName()).replace(' ', '_'));
-			stringBuilder.append(String.format("%-25s|", c.getLanguage()).replace(' ', '_'));
-			stringBuilder.append(String.format("%-10s|", c.getCurrency()).replace(' ', '_'));
-			
+			stringBuilder.append(String.format("|%-10s|", results.get(i).getId()).replace(' ', '_'));
+			stringBuilder.append(String.format("%-50s|", results.get(i).getName()).replace(' ', '_'));
+			stringBuilder.append(String.format("%-50s|", results.get(i).getLanguage()).replace(' ', '_'));
+			stringBuilder.append(String.format("%-10s|", results.get(i).getCurrency()).replace(' ', '_'));
+			stringBuilder.append(String.format("%-10s|", results.get(i).getTaxRate() ).replace(' ', '_'));
 
 			stringBuilder.append("\n");
 		}
@@ -110,13 +192,15 @@ public class DepartmentManager {
 	}
 	public void updateCountry() {
 		try {
-		System.out.println("Enter country name ");
+			
+		System.out.println("Enter Country Name ");
 		String name  = sc.next();
 		Query query =em.createQuery("Select id from Country c where c.name LIKE :name");
 		query.setParameter("name", name);
 		
 		List id  = query.getResultList();
 		em.getTransaction().begin();
+		
 		Country c  = em.find(Country.class,id.get(0));
 		
 		/* Now we need to update the code*/
@@ -124,8 +208,9 @@ public class DepartmentManager {
 		String name_old  = c.name;
 		String language_old  = c.language;
 		String currency_old   =  c.currency;
+		double text_old  = c.taxRate;
 		
-		updatable(name_old,language_old,currency_old,c);
+		updatable(name_old,language_old,currency_old,text_old,c);
 		System.out.println("Successfully Updated");
 		em.getTransaction().commit();
 		
@@ -138,39 +223,82 @@ public class DepartmentManager {
 		
 	}
 	
-	private static  void updatable(String name,String language,String currency,Country co)
+	private static  void updatable(String name,String language,String currency,double tax,Country co) throws IOException
 	{
 		
 		int  count  =0;
-		System.out.println("Please Enter the new name if you do not want to change the name either leave it blank or type no ");
-		String new_name =  sc.next();
+		boolean is_valid  =  true;
+		do {
+		System.out.println("Please Enter the New Name. if you do not want to Change the Name either leave it blank or type no ");
+		String new_name =  r.readLine();
+		if (ischeckChar(new_name) ==true) {
 		if (new_name.equals(" ")|| new_name.toLowerCase().equals("no")) {
 			
 			co.setName(name);
 			count++;
+			is_valid = false;
 		}else {
 			co.setName(new_name);
 			count++;
+			is_valid  = false;
 		}
+		}else {
+			System.out.println("Invalid Input!");
+		}}while(is_valid);
 		
-		System.out.println("Please Enter the new language if you do not want to change the language either leave it blank or type no ");
-		String new_language=  sc.next();
+		boolean is_valid_ =true;
+		do {
+		System.out.println("Please Enter the New Language. if you do not want to Change the Language either leave it blank or type no ");
+		String new_language=  r.readLine();
+		if(ischeckChar(new_language)==true) {
 		if (new_language.equals(" ")|| new_language.toLowerCase().equals("no")) {
 			
 			co.setLanguage(language);
+			is_valid_ =  false;
 		}else {
 			co.setLanguage(new_language);
+			is_valid_ =  false;
 		}
+		}else {
+			System.out.println("Invalid Input");
+		}
+		}while(is_valid_);
 		
-		
-		System.out.println("Please Enter the new Currency if you do not want to change the currency either leave it blank or type no ");
-		String new_curreny =  sc.next();
+		boolean is_valid_input__ =true;
+		do {
+		System.out.println("Please Enter the New Currency. if you do not want to Change the Currency either leave it blank or type no ");
+		String new_curreny =  r.readLine();
+		if (new_curreny.length() < 3 || ischeckChar(new_curreny)==true) {
 		if (new_curreny.equals(" ") || new_curreny.toLowerCase().equals("no")) {
 			
 			co.setCurrency(currency);
+			is_valid_input__ =  false;
 		}else {
 			co.setCurrency(new_curreny);
+			is_valid_input__   = false;
+			
+		}}else {
+			System.out.println("Invalid Input !");
 		}
+		}while(is_valid_input__);
+		
+		boolean  is_valid__input  =  true;
+		do {
+		System.out.println("Please Enter the New Tax Rate. if you do not want to Change the Tax Rate either leave it blank or type no ");
+		String Tax_rate =  r.readLine();
+		try {
+		if (Tax_rate.equals(" ") || Tax_rate.toLowerCase().equals("no")) {
+			
+			co.setTaxRate(tax);
+			is_valid__input =false;
+		}else {
+			co.setTaxRate(Double.parseDouble(Tax_rate));
+			is_valid__input =false;
+		}
+		}catch(Exception e) {
+			System.out.println("Invalid Tax Rate");
+		}
+		}while(is_valid__input);
 		
 		
 	}
@@ -179,7 +307,7 @@ public class DepartmentManager {
 		
 		try {
 			
-			System.out.println("Enter the name of the country ! You want to delete");
+			System.out.println("Enter the Name of the Country ! You want to delete");
 			String name =  sc.next();
 			Query query  = em.createQuery("Select id from Country c where c.name LIKE :name");
 			query.setParameter("name", name);
@@ -190,160 +318,108 @@ public class DepartmentManager {
 			
 			if(!results.isEmpty())
 			{
-				System.out.println("Sorry You cannot Delete this Country because it has Entry in Location Table ");
+				System.out.println("Sorry, You can’t Delete this Country because it has Entity in Location Table ");
 			}else {
 			
-			// when we delete the country we should delete it from the location table
-			//lets start deleting it using the JPA method of remove 
+			
 			em.getTransaction().begin();
 			Country c_name = em.find(Country.class, id.get(0));
-			
-			em.remove(c_name);
-			
-						
+		    em.remove(c_name);
 			em.getTransaction().commit();
 			System.out.println("Successfully deleted");
 			}
 		}catch(Exception e )
 		{
 			e.printStackTrace();
-			
+		
 		}
 		
 	}
 
+	
+	public void country(EntityManager em) throws IOException {
 
-
-	public void addLocation() {
-		try {
-			// First ask user for which country the user want to enter the location
-			TypedQuery<Country> query = em.createQuery("SELECT c FROM Country c", Country.class);
-			List<Country> results = query.getResultList();
-			
-			HashMap<String, Integer> map = new HashMap<String, Integer>();
-			for(int i =0;i<results.size();i++)
-			{
-				System.out.println(results.get(i).id+"  "+results.get(i).name +"");
-				map.put("id"+i, results.get(i).id);
-				
-			}
-			
-			System.out.println("If the country is not present in the list you can add ? enter yes or no");
-			
-			String answer  =sc.next();
-			
-			if(answer.toLowerCase().equals("yes")) {
-				System.out.println("hello");
-				addCountry();
-				addLocation();
-			}else {
-			System.out.println("Please Enter the number of the coutry for which  you want to insert location");
-				
-			
-			int number  =  sc.nextInt();
-			if(!map.containsValue(number))
-			{
-				System.out.println("Sir You Entered Wrong number please verify again!");
-				
-			}
-			
-			//
-			Location co = new Location();
-			
-			boolean invalidInput = true;
-			String name;
-			
-			do {
-				System.out.println("Add Location Details");
-				System.out.print("Location name: ");
-				name = sc.next();
-				co.setName(name);
-		    	try {
-		    		em.createQuery("from Location where name = '" + name + "'").getSingleResult();    		
-		    		System.out.println("Location already exists in Database.");
-		    		boolean isRetry = true;
-		    		do {
-			    		System.out.print("Try again? (y/n) ");
-			    		switch(sc.nextLine().charAt(0)) {
-			    		case 'n':
-			    			return;
-			    		case 'y':
-			    			isRetry = true;
-			    			break;
-			    		default:
-			    			isRetry = false;
-			    			break;
-			    			}
-		    		} while(!isRetry);
-		    		
-		    	} catch (NoResultException nre) {
-		    		invalidInput = false;
-		    	}
-		    	catch (Exception e){
-		    		System.out.println(e.getMessage());
-		    	}
-			} while (invalidInput);
-			BufferedReader r = new BufferedReader(new InputStreamReader(System.in));	
-			System.out.print("Location Address: ");
-			String l_address = r.readLine();
-			co.setAddress(l_address);
-			
-			
-		
-			System.out.print("Location detailes: ");
-			
-			String l_detailes = r.readLine();
-			co.setDetails(l_detailes);
-			
-			co.setCountryId(number);
-			em.getTransaction().begin();
-			em.persist(co);
-			em.getTransaction().commit();
-		
-			Location dbObject = em.createQuery("from Location c where c.name='" + name + "'",Location.class).getSingleResult();
-			
-			System.out.println(dbObject.toString());
-			
-			}
-			
-			
-		
-			  
-			
-		}catch(Exception e )
-		{
-			e.printStackTrace();
-		}
+    
+    	System.out.println("Choose the Field that you want to see, Please Enter the Number only ");
+    	System.out.println("1. Add ");
+    	System.out.println("2. View ");
+    	System.out.println("3. update ");
+    	System.out.println("4. Delete ");
+    	
+    	String  result  =r.readLine();
+    	
+    	switch(Integer.parseInt(result) ){
+    		
+    	case 1:
+    		addCountry(); break;
+    	case 2:
+    		viewCountry();break;
+    	case 3:
+    		updateCountry();break;
+    		
+    	case 4:
+    		deleteCountry();
+    	
+    		break;
+    		
+    	default:
+    			System.out.println("Error");
+    			
+    	}
 	}
 	
-	public void viewLocation() {
-		StringBuilder stringBuilder = new StringBuilder();
-		System.out.println("_____View all available Location______");
-		stringBuilder.append(String.format("%150s\n", "").replace(' ', '_'));
-		stringBuilder.append(String.format("|%-10s|%-25s|%-25s|%-25s|%-25s|\n", "LOCATION ID","NAME", "ADDRESS", "DETAILES","COUNTRY ID").replace(' ', '_'));
+	public void department(EntityManager em) {
 		
-		for (Location c : location) {
-			
-			stringBuilder.append(String.format("|%10d|", c.getId()).replace(' ', '_'));
-			stringBuilder.append(String.format("%-25s|", c.getName()).replace(' ', '_'));
-			stringBuilder.append(String.format("%-25s|", c.getAddress()).replace(' ', '_'));
-			stringBuilder.append(String.format("%-25s|", c.getDetails()).replace(' ', '_'));
-			stringBuilder.append(String.format("%-25s|", c.getCountryId()).replace(' ', '_'));
+		System.out.println("Choose the Field that you want to go, Please Enter the Number only ");
+    	System.out.println("1. Add ");
+    	System.out.println("2. View ");
+    	System.out.println("3. Update ");
+    	System.out.println("4. Delete ");
+    	
+    	int  result  = sc.nextInt();
+    	
+    	
+	}
+	
 
-			stringBuilder.append("\n");
+	public void ask_what(EntityManager em) throws IOException
+	{
+		
+		System.out.println("Please Enter the Number in which you want to do Transaction by looking in the option");
+		System.out.println("1. Country ");
+		System.out.println("2. Location ");
+		System.out.println("3. Department ");
+		int number  =  sc.nextInt();
+		
+		switch(number) {
+		
+		case 1 : country(em); d_name="Country"; break;
+		
+		default: System.out.println("Invalid Number. Try again ! ");
+		
 		}
 		
-		System.out.println(stringBuilder.toString());
+		
+		
+	}
+	public void call(EntityManager em) throws IOException {
+		
+    	ask_what(em);
+    	
+    	String answer ;
+		do {
+    		System.out.println("Do you want to see the Changes in this Table or Go back to Main Menu ? Enter yes or no");
+        	answer  = r.readLine();
+    
+        	if(answer.toLowerCase().equals("yes")) {
+        if (DepartmentManager.d_name.equals("Country")) {		
+    		country(em);
+    		
+        }
+        	}else {
+        	ask_what(em);
+        	}
+    	}while(answer.toLowerCase() !="");
 	}
 
 }
-	
-	
-
-
-
-
-
-
-
-
